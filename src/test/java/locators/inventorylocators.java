4947ec3dc39ff2057b1d69f7f5cb1384c8525a2d
efeisliyen0 <efe.isliyen@testinium.com>
2026-07-09 10:05:02 +0300

    Refactor inventory steps with step implementation architecture

--- src/test/java/locators/inventorylocators.java
+++ src/test/java/locators/inventorylocators.java
@@ -3,10 +3,13 @@ package locators;
 import org.openqa.selenium.By;
 
 public class inventorylocators {
+
+    public static final By PRODUCTS_TITLE = By.className("title");
     public static final By MENU_BUTTON = By.id("react-burger-menu-btn");
     public static final By LOGOUT_BUTTON = By.id("logout_sidebar_link");
-    public static final By PRODUCTS_TITLE = By.className("title");
-    public static final By PRODUCT_DETAIL_TITLE = By.className("inventory_details_name");
-    public static By getProduct(String productName) {
-        return By.xpath("//a[.//div[text()='" + productName + "']]");
-    }}
+    public static By getProduct(String productName)
+    {return By.xpath("//a[.//div[text()='" + productName + "']]");}
+    public static By getProductDetailTitle(String productName) {
+        return By.xpath("//div[@class='inventory_details_name' and text()='" + productName + "']");
+    }
+}
