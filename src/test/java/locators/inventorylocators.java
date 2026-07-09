edbde41814132d3d3d21e6081b40a5d0a8a083b2
efeisliyen0 <efe.isliyen@testinium.com>
2026-07-09 08:47:22 +0300

    Implement login, logout and product detail tests with helper-locator framework

--- src/test/java/locators/inventorylocators.java
+++ src/test/java/locators/inventorylocators.java
@@ -6,4 +6,7 @@ public class inventorylocators {
     public static final By MENU_BUTTON = By.id("react-burger-menu-btn");
     public static final By LOGOUT_BUTTON = By.id("logout_sidebar_link");
     public static final By PRODUCTS_TITLE = By.className("title");
-}
+    public static final By PRODUCT_DETAIL_TITLE = By.className("inventory_details_name");
+    public static By getProduct(String productName) {
+        return By.xpath("//a[.//div[text()='" + productName + "']]");
+    }}
--- src/test/java/steps/InventorySteps.java
+++ src/test/java/steps/InventorySteps.java
@@ -5,6 +5,7 @@ import helpers.elementhelper;
 import locators.inventorylocators;
 
 public class InventorySteps {
+
     @Step("User clicks menu button")
     public void clickMenuButton() {
         elementhelper.click(inventorylocators.MENU_BUTTON);
@@ -14,8 +15,18 @@ public class InventorySteps {
         elementhelper.click(inventorylocators.LOGOUT_BUTTON);
     }
     @Step("User should see products page")
-    public void verifyProductsPage() {
-        String title = elementhelper.getText(inventorylocators.PRODUCTS_TITLE);
-        assert title.equals("Products");
+    public void verifyProductsPage() {String title = elementhelper.getText(inventorylocators.PRODUCTS_TITLE);assert title.equals("Products");
     }
-}
+
+
+        @Step("User clicks <productName> product")
+        public void clickProduct(String productName) {
+        elementhelper.click(inventorylocators.getProduct(productName));}
+        @Step("User should see product detail page")
+        public void verifyProductDetailPage() { assert elementhelper.isDisplayed(inventorylocators.PRODUCT_DETAIL_TITLE);
+        }
+    }
+
+
+
+
