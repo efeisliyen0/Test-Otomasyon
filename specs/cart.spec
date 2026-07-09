80fab69da73f504b56ade6fac7c238bc99b06c46
bilge.erol <bilgeerol02@gmail.com>
2026-07-09 09:54:49 +0300

    refactor: AssertJ entegrasyonu yapildi ve ortak step mimarisi kuruldu

---
+++ specs/cart.spec
@@ -0,0 +1,9 @@
+# Cart Tests
+
+## Add Backpack to Cart and Verify
+* User enters "standard_user" into "username" field
+* User enters "secret_sauce" into "password" field
+* User clicks "login" button
+* User clicks "add to cart backpack" button
+* User clicks "shopping cart" button
+* User should see "Sauce Labs Backpack" in the cart
--- src/test/java/stepimplementation/CartStepsİmplementation.java
+++ src/test/java/stepimplementation/CartStepsİmplementation.java
@@ -1,4 +1,45 @@
 package stepimplementation;
 
+import com.thoughtworks.gauge.Step;
+import helpers.elementhelper;
+import locators.cartlocators;
+import locators.loginlocators;
+import org.openqa.selenium.By;
+import org.assertj.core.api.Assertions;
+
 public class CartStepsİmplementation {
+
+    private By getLocator(String locatorName) {
+        switch (locatorName.toLowerCase()) {
+            case "username": return loginlocators.USERNAME_INPUT;
+            case "password": return loginlocators.PASSWORD_INPUT;
+            case "login": return loginlocators.LOGIN_BUTTON;
+
+            case "add to cart backpack": return cartlocators.ADD_TO_CART_BACKPACK;
+            case "shopping cart": return cartlocators.SHOPPING_CART_LINK;
+            case "cart title": return cartlocators.CART_TITLE;
+
+            default: throw new IllegalArgumentException("Locator bulunamadi veya yanlis yazildi: " + locatorName);
+        }
+    }
+
+    @Step("User enters <text> into <locatorName> field")
+    public void enterTextIntoField(String text, String locatorName) {
+        By locator = getLocator(locatorName);
+        elementhelper.sendKeys(locator, text);
+    }
+
+    @Step("User clicks <locatorName> button")
+    public void clickButton(String locatorName) {
+        By locator = getLocator(locatorName);
+        elementhelper.click(locator);
+    }
+
+    @Step("User should see <text> in the cart")
+    public void verifyItemInCart(String expectedText) {
+        String actualText = elementhelper.getText(cartlocators.INVENTORY_ITEM_NAME);
+        Assertions.assertThat(actualText)
+                .withFailMessage("Sepetteki urun ismi eslesmedi! Beklenen: " + expectedText + " ama sayfada gorunen: " + actualText)
+                .isEqualTo(expectedText);
+    }
 }
