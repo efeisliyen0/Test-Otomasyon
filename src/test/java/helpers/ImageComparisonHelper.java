7ccb71e0455caed0223d1decd598a3cf60289171
efeisliyen <efeisliyenn@gmail.com>
2026-07-14 08:39:28 +0300

    düzenleme

--- src/test/java/helpers/ImageComparisonHelper.java
+++ src/test/java/helpers/ImageComparisonHelper.java
@@ -8,99 +8,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ImageComparisonHelper {
-
-
     public static void compare(String fileName) {
-
-
-        try {
-
-
-            File baselineFile = new File(
-                    ".gauge/screenshots/visual/baseline/"
-                            + fileName + ".png"
-            );
-
-
-            File actualFile = new File(
-                    ".gauge/screenshots/visual/actual/"
-                            + fileName + ".png"
-            );
-
-
-            BufferedImage baselineImage =
-                    ImageIO.read(baselineFile);
-
-
-            BufferedImage actualImage =
-                    ImageIO.read(actualFile);
-
-
-
-            boolean result =
-                    compareImages(
-                            baselineImage,
-                            actualImage
-                    );
-
-
-
-            assertThat(result)
-                    .as("Screenshots are different!")
-                    .isTrue();
-
-
-
-        } catch(Exception e){
-
-            throw new RuntimeException(
-                    "Image comparison failed : "
-                            + e.getMessage()
-            );
-
+        try {File baselineFile = new File(".gauge/screenshots/visual/baseline/" + fileName + ".png");
+            File actualFile = new File(".gauge/screenshots/visual/actual/" + fileName + ".png");
+            BufferedImage baselineImage = ImageIO.read(baselineFile);
+            BufferedImage actualImage = ImageIO.read(actualFile);
+            boolean result = compareImages(baselineImage,actualImage);
+            assertThat(result).as("Screenshots are different!").isTrue();
+        } catch(Exception e){throw new RuntimeException("Image comparison failed : " + e.getMessage());
         }
-
-
     }
-
-
-
-    private static boolean compareImages(
-            BufferedImage img1,
-            BufferedImage img2
-    ){
-
-
-        if(img1.getWidth()!=img2.getWidth()
-                || img1.getHeight()!=img2.getHeight()){
-
-            return false;
-
+    private static boolean compareImages(BufferedImage img1, BufferedImage img2
+    ){if(img1.getWidth()!=img2.getWidth() || img1.getHeight()!=img2.getHeight()){return false;}
+        for(int x=0; x<img1.getWidth(); x++){for(int y=0; y<img1.getHeight(); y++){if(img1.getRGB(x,y) != img2.getRGB(x,y)){return false;}}
         }
-
-
-
-        for(int x=0; x<img1.getWidth(); x++){
-
-
-            for(int y=0; y<img1.getHeight(); y++){
-
-
-                if(img1.getRGB(x,y)
-                        != img2.getRGB(x,y)){
-
-
-                    return false;
-
-                }
-
-            }
-
-        }
-
-
         return true;
-
     }
 
 
--- src/test/java/steps/VisualSteps.java
+++ src/test/java/steps/VisualSteps.java
@@ -4,31 +4,11 @@ import com.thoughtworks.gauge.Step;
 import stepimplementation.VisualStepsImplementation;
 
 public class VisualSteps {
-
     VisualStepsImplementation visualStepsImplementation = new VisualStepsImplementation();
-
-
     @Step("User takes baseline screenshot as <fileName>")
-    public void takeBaselineScreenshot(String fileName){
-
-        visualStepsImplementation.takeBaselineScreenshot(fileName);
-
-    }
-
-
+    public void takeBaselineScreenshot(String fileName){visualStepsImplementation.takeBaselineScreenshot(fileName);}
     @Step("User takes actual screenshot as <fileName>")
-    public void takeActualScreenshot(String fileName){
-
-        visualStepsImplementation.takeActualScreenshot(fileName);
-
-    }
-
-
+    public void takeActualScreenshot(String fileName){visualStepsImplementation.takeActualScreenshot(fileName);}
     @Step("User compares screenshot with baseline <fileName>")
-    public void compareScreenshot(String fileName){
-
-        visualStepsImplementation.compareScreenshot(fileName);
-
-    }
-
+    public void compareScreenshot(String fileName){visualStepsImplementation.compareScreenshot(fileName);}
 }
