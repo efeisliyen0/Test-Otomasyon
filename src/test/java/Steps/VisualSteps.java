aaf8ee6d58b792f5f3146ced0d297ee7f5b3b182
efeisliyen <efeisliyenn@gmail.com>
2026-07-16 13:59:53 +0300

    değişiklikler

--- src/test/java/Steps/VisualSteps.java
+++ src/test/java/Steps/VisualSteps.java
@@ -1,14 +1,29 @@
 package Steps;
 
 import com.thoughtworks.gauge.Step;
+import helpers.ImageComparisonHelper;
+import helpers.ScreenshotHelper;
 
 public class VisualSteps {
 
-    @Step("User takes screenshot and saves as <name>")
-    public void takeScreenshot(String name) {
+    private static final String BASELINE_PATH =
+            ".gauge/screenshots/visual/baseline/";
+
+    private static final String ACTUAL_PATH =
+            ".gauge/screenshots/visual/actual/";
+
+    @Step("User takes baseline screenshot as <fileName>")
+    public void takeBaselineScreenshot(String fileName) {
+        ScreenshotHelper.takeScreenshot(BASELINE_PATH, fileName);
     }
 
-    @Step("User compares screenshot <actual> with <baseline>")
-    public void compareScreenshots(String actual, String baseline) {
+    @Step("User takes actual screenshot as <fileName>")
+    public void takeActualScreenshot(String fileName) {
+        ScreenshotHelper.takeScreenshot(ACTUAL_PATH, fileName);
+    }
+
+    @Step("User compares screenshot with baseline <fileName>")
+    public void compareScreenshot(String fileName) {
+        ImageComparisonHelper.compare(fileName);
     }
 }
